ebeb9a957de666eb141139c4048ff5bf238a05fb
fsabirov <sabirovfarit@gmail.com>
2022-10-25 11:46:39 +0300

    ejs work

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const exptress = require('express');
 const router = exptress.Router();
 
 router.get('/', (req, res) => {
-  res.render('index',{title:'Главная'});
+  res.render('index',{title:'Book'});
 });
 
 module.exports = router;
